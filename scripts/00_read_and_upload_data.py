563af7e9bfa2c7e808591ad6a27418cb626c8d5b
Arnau <arnau@ersilia.io>
2025-10-30 11:01:02 +0100

    reading and uploading lead-like

--- scripts/00_read_and_upload_data.py
+++ scripts/00_read_and_upload_data.py
@@ -29,13 +29,33 @@ root = '.'
 ##### REAL SAMPLE ######
 ########################
 
-print("Splitting REAL Sample!")
-PATH_TO_DATA = "/home/acomajuncosa/Downloads/2025.02_Enamine_REAL_DB_1B.cxsmiles.bz2"
+# print("Splitting REAL Sample!")
+# PATH_TO_DATA = "/home/acomajuncosa/Downloads/2025.02_Enamine_REAL_DB_1B.cxsmiles.bz2"
+
+# for c, chunk in enumerate(pd.read_csv(PATH_TO_DATA, compression="infer", chunksize=10_000_000, dtype_backend="pyarrow", sep='\t', usecols=["smiles", "id"])):
+
+#     # Set filename
+#     filename = f"Enamine_REAL_Sample_{str(c).zfill(3)}.csv.zst"
+    
+#     # Save chunk
+#     chunk.to_csv(os.path.join(root, "..", "tmp", filename), index=False, compression={"method": "zstd", "level": 10})
+
+#     # Upload to Google Drive and remove file
+#     upload(os.path.join(root, "..", "tmp", filename), "../service.json", folder_id="1P3DSLRpD0GL8th2B78b7OauEXp3I2ZY0")
+#     os.remove(os.path.join(root, "..", "tmp", filename))
+
+
+########################
+###### LEAD-LIKE #######
+########################
+
+print("Splitting REAL Lead-Like!")
+PATH_TO_DATA = "/home/acomajuncosa/Downloads/Enamine_REAL_lead-like_cxsmiles.cxsmiles.bz2"
 
 for c, chunk in enumerate(pd.read_csv(PATH_TO_DATA, compression="infer", chunksize=10_000_000, dtype_backend="pyarrow", sep='\t', usecols=["smiles", "id"])):
 
     # Set filename
-    filename = f"Enamine_REAL_Sample_{str(c).zfill(3)}.csv.zst"
+    filename = f"Enamine_REAL_LeadLike_{str(c).zfill(3)}.csv.zst"
     
     # Save chunk
     chunk.to_csv(os.path.join(root, "..", "tmp", filename), index=False, compression={"method": "zstd", "level": 10})
@@ -43,8 +63,3 @@ for c, chunk in enumerate(pd.read_csv(PATH_TO_DATA, compression="infer", chunksi
     # Upload to Google Drive and remove file
     upload(os.path.join(root, "..", "tmp", filename), "../service.json", folder_id="1P3DSLRpD0GL8th2B78b7OauEXp3I2ZY0")
     os.remove(os.path.join(root, "..", "tmp", filename))
-
-
-########################
-###### LEAD-LIKE #######
-########################
